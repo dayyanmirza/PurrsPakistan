d80cbe63bd1fce5cef8607984ca37171f76ebb9e
dayyanmirza <dayyanmirza15@gmail.com>
2024-07-23 11:49:47 +0100

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,26 @@
-const Home = () => {
+import React from "react";
+import Fade from "react-reveal/Fade";
 
-    return ( 
-        <div className="home">
-        </div>
-     );
-}
+// https://medium.com/@jakintemi/how-to-use-fade-effect-in-your-react-app-e7a72f7e1db8
+
+const Home = () => {
+    return (
+      <div className="home">
+        <Fade bottom duration={1500}>
+          <div className="start">
+            <h1>Purrs of Pakistan</h1>
+            <p>
+              Welcome to Purrs of Pakistan, a dedicated platform for the care and rescue of cats in need.
+              Our mission is to provide life-saving treatments, rescue operations, and necessary surgeries for cats who require our help.
+            </p>
+            <div className="learn-button">
+            <button>Learn more</button>
+            </div>
+          </div>
+        </Fade>
+      </div>
+    );
+  };
+
 
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,11 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>Purrs Pakistan</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px'
-                }}>New Blog</a>
+                <a href="/">About</a>
+                <a href="/">Cats</a>
+                <a href="/">Donate</a>
             </div>
         </nav>
      );
